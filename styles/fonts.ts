4756b4113a8c1cccc3584b33fa4ff8e2433b613a
winning godspower <winninggodspower@gmail.com>
2025-04-28 12:46:20 +0100

    update: specify poppins subset font

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,8 +1,8 @@
 import { Inter, Poppins } from 'next/font/google'
-import localFont from 'next/font/local'
  
 // define your variable fonts
 const poppins = Poppins({
+  subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
 })
 const inter = Inter({
